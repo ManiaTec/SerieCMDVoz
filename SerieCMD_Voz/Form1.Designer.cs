af79d969f3c3594c006b527ac352abf56ab6f991
ManiaTecja.tk <you@example.com>
2018-10-22 20:44:49 -0300

    Source do tutorial

---
+++ SerieCMD_Voz/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace SerieCMD_Voz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(77, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Off";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(123, 126);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Porta COM:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(58, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 69);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ManiaTec";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(89, 94);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(126, 25);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "maniatecja.tk";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Comando de Voz ManiaTec";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ SerieCMD_Voz/Form1.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Speech.Recognition;
+using System;
+using System.IO.Ports;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+
+namespace SerieCMD_Voz
+{
+    public partial class Form1 : Form
+    {
+        //Configura o sitema de comando de voz
+        private SpeechRecognitionEngine sre = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("pt-BR"));
+        //Reconhecendo ?
+        private bool ligado = false;
+        //COM Port selecionada
+        private String compt;
+        public Form1()
+        {
+            InitializeComponent();
+            //Pega as portas serial disponiveis e as coloca na Combobox
+            string[] ports = SerialPort.GetPortNames();
+            comboBox1.DataSource = ports;
+            //Informa as palavreas a serem reconhecidas e configura a gramática
+            Choices Palavras_Rec = new Choices();
+            Palavras_Rec.Add(new string[] {"Oi", "Ligar luz", "Desligar luz" });
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(Palavras_Rec);
+            Grammar g = new Grammar(gb);
+            sre.LoadGrammar(g);
+            //Informa a função que recebe a palavra reconhecida
+            sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(quando_reconhecer);
+            //Faz com que use o microfone padrão
+            sre.SetInputToDefaultAudioDevice();
+        }
+        //Quando reconhecer ↓
+        void quando_reconhecer(object sender, SpeechRecognizedEventArgs e)
+        {
+            //Só concidera quando tiver confiança de 70% quado reconhecer
+            if (e.Result.Confidence > (Convert.ToDouble(70)/100)) {
+                //Inicia uma tentativa de executar oque foi solicitado por voz
+                try
+                {
+                    //Passa o texto reconhecido para variável
+                    var resp = e.Result.Text;
+                    //Se o texto for "Oi" ↓
+                    if (resp == "Oi") {
+                        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+                        //Volume de resposta
+                        synthesizer.Volume = 100;  // 0...100
+                        //Velocidade de resposta
+                        synthesizer.Rate = 0;     // -10...10
+                        //Oque será dito
+                        synthesizer.Speak("Olá");
+                    }
+                    else /*Se o texto for "Ligar luz" →*/ if (resp == "Ligar luz") {
+                        //Seleciona a porta COM
+                        SerialPort port = new SerialPort(compt, 9600);
+                        //Tenta enviar o comando por serial
+                        try
+                        {
+                            port.Open();
+                            port.Write("ligar");
+                            //Foi
+                        }
+                        catch (Exception ex)
+                        {
+                            //Erro
+                            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+                            synthesizer.Volume = 100;  // 0...100
+                            synthesizer.Rate = 0;     // -10...10
+                            synthesizer.Speak("Erro COM");
+                            MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        port.Close();
+                    }
+                }
+                catch { }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Verifica se está reconhecendo ou não
+            if (!ligado)
+            {
+                //Estava desligado e vai ser ligado 
+                compt = comboBox1.Text;
+                button1.Text = "On";
+                ligado = true;
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            else
+            {
+                //Estava ligado vai ser desligado
+                button1.Text = "Off";
+                ligado = false;
+                sre.RecognizeAsyncStop();
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://maniatecja.tk/");
+        }
+    }
+}
